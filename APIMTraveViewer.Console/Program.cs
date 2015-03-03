fb9a841db042315f5776ffdd3736f6bc1ce6c1a6
Lawrence Gripper <lawrencegripper@hotmail.co.uk>
2015-03-03 16:06:33 +0000

    Tweaked output from console app.

--- APIMTraveViewer.Console/Program.cs
+++ APIMTraveViewer.Console/Program.cs
@@ -14,10 +14,15 @@ namespace APIMTraveViewer.CmdLine
     {
         class Options
         {
-            [Option('r', "request", Required = true, HelpText = "Input files to be processed.")]
-            public Uri RequestUri { get; set; }
 
-            [Option('f', "file", Required = true, HelpText = "Output folder where traces will be saved.")]
+            [ParserState]
+            public IParserState LastParserState { get; set; }
+
+
+            [Option('u', "url", Required = false, HelpText = "Input files to be processed.")]
+            public string RequestUri { get; set; }
+
+            [Option('f', "file", Required = false, HelpText = "Output folder where traces will be saved.")]
             public string OutputFolder { get; set; }
 
             [HelpOption]
@@ -34,24 +39,28 @@ namespace APIMTraveViewer.CmdLine
             if (Parser.Default.ParseArguments(args, options))
             {
 
-                Console.WriteLine("Request to Trace: " + options.RequestUri.ToString());
+                Console.WriteLine("Request to Trace: " + options.RequestUri);
                 Console.WriteLine("Output folder for result: " + options.OutputFolder);
 
-                IssueRequest(options.RequestUri.ToString(), options.OutputFolder).Wait();
+                IssueRequest(options.RequestUri, options.OutputFolder).Wait();
             }
+
+            Console.ReadLine();
         }
 
         private static async Task IssueRequest(string requestUri, string folder)
         {
-            var n = string.Format("text-{0:yyyy-MM-dd_hh-mm-ss-tt}.json", DateTime.Now);
-            var filePath = Path.Combine(folder, n);
-            using (StreamWriter outputFile = new StreamWriter(filePath))
+
+            //Issue a request and get trace
+            EchoApiService echoTester = new EchoApiService();
+            var result = await echoTester.GetRequestWithTrace(requestUri);
+
+            var trace = string.Format("trace-{0:yyyy-MM-dd_hh-mm-ss-tt}.json", DateTime.Now);
+            var traceFilePath = Path.Combine(folder, trace);
+
+            using (StreamWriter outputFile = new StreamWriter(traceFilePath))
             {
                 //Could be MVVM but feels like overkill given the simplicity.
-                EchoApiService echoTester = new EchoApiService();
-
-                //Issue a request and get trace
-                var result = await echoTester.GetRequestWithTrace(requestUri);
 
                 //Show errors, if any
                 if (result.HasError)
@@ -59,17 +68,21 @@ namespace APIMTraveViewer.CmdLine
                     await outputFile.WriteLineAsync(string.Format("An error occured '{0}' Exception Details: '{1}'", result.ErrorDetails.Message, result.ErrorDetails.ToString()));
                 }
 
-                await outputFile.WriteLineAsync("Trace");
                 await outputFile.WriteAsync(result.TraceString);
-                await outputFile.WriteLineAsync("Body");
-                await outputFile.WriteAsync(result.BodyContent);
-                await outputFile.WriteLineAsync("Headers");
-                await outputFile.WriteAsync(result.ResponseMessage.Headers.ToString());
-
             }
 
-
-
+            var request = string.Format("request-{0:yyyy-MM-dd_hh-mm-ss-tt}.txt", DateTime.Now);
+            var requestFilePath = Path.Combine(folder, request);
+            using (StreamWriter outputFile = new StreamWriter(requestFilePath))
+            {
+                await outputFile.WriteLineAsync("-----------RequestUrl-----------");
+                await outputFile.WriteAsync(requestUri);
+                await outputFile.WriteLineAsync("-----------Body-----------");
+                await outputFile.WriteAsync(result.BodyContent);
+                await outputFile.WriteLineAsync("-----------Headers-----------");
+                await outputFile.WriteAsync(result.ResponseMessage.Headers.ToString());
+            }
         }
+
     }
 }
